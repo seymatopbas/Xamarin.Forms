3f30b4d0f02a4dc3ccb3f1f92ab9faf6f3c3338a
E.Z. Hart <hartez@gmail.com>
2017-06-28 14:51:08 -0600

    Repro

---
+++ Xamarin.Forms.ControlGallery.iOS/_41269Renderer.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Text;
+using System.Threading;
+using Xamarin.Forms;
+using Xamarin.Forms.ControlGallery.iOS;
+using Xamarin.Forms.Controls.Issues;
+using Xamarin.Forms.Internals;
+using Xamarin.Forms.Platform.iOS;
+
+//[assembly: ExportRenderer(typeof(Bugzilla41269._41269Page), typeof(_41269Renderer))]
+namespace Xamarin.Forms.ControlGallery.iOS
+{
+	public class _41269Renderer : PageRenderer
+	{
+		static int s_count;
+
+		~_41269Renderer()
+		{
+			Interlocked.Decrement(ref s_count);
+			Log.Warning("41269 Renderer", $"_41269 renderer instance count is: {s_count}");
+		}
+
+		public _41269Renderer()
+		{
+			Interlocked.Increment(ref s_count);
+			Log.Warning("41269 Renderer", $"_41269 renderer instance count is: {s_count}");
+		}
+	}
+}
---
+++ Xamarin.Forms.Controls.Issues/Xamarin.Forms.Controls.Issues.Shared/Bugzilla41269.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Threading;
+using Xamarin.Forms.CustomAttributes;
+using Xamarin.Forms.Internals;
+
+#if UITEST
+using Xamarin.UITest;
+using NUnit.Framework;
+using Xamarin.Forms.Core.UITests;
+#endif
+
+namespace Xamarin.Forms.Controls.Issues
+{
+//#if UITEST
+//	[Category(UITestCategories.)]
+//#endif
+
+	[Preserve(AllMembers = true)]
+	[Issue(IssueTracker.Bugzilla, 941269, "", PlatformAffected.All)]
+	public class Bugzilla41269 : TestNavigationPage 
+	{
+		protected override void Init()
+		{
+			_rendererCount = new Label {Text = "..."};
+			_pageCount = new Label { Text = "..." };
+			_pvcCount = new Label { Text = "..." };
+
+			Log.Listeners.Add(
+				new DelegateLogListener((c, m) => Device.BeginInvokeOnMainThread(() =>
+				{
+					if (c == "41269 Page")
+					{
+						_pageCount.Text = m;
+					}
+
+					if (c == "41269 Renderer")
+					{
+						_rendererCount.Text = m;
+					}
+
+					if (c == "41269 PVC")
+					{
+						_pvcCount.Text = m;
+					}
+				})));
+
+			PushAsync(Root());
+		}
+
+		Label _rendererCount;
+		Label _pageCount;
+		Label _pvcCount;
+
+		ContentPage Root()
+		{
+			var button = new Button { Text = "Push custom page" };
+			button.Clicked += (sender, args) => Navigation.PushAsync(new _41269Page(), false);
+
+			var layout = new StackLayout()
+			{
+				Children = { _pageCount, _rendererCount, _pvcCount, button }
+			};
+
+			var result = new ContentPage { Content = layout };
+
+			result.Appearing += (sender, args) =>
+			{
+				GC.Collect();
+				GC.WaitForPendingFinalizers();
+			};
+
+			return result;
+		}
+
+		[Preserve(AllMembers = true)]
+		public class _41269Page : ContentPage
+		{
+			static int s_count;
+
+			public _41269Page()
+			{
+				Title = "41269 Page";
+				Interlocked.Increment(ref s_count);
+				Log.Warning("41269 Page", $"_41269 page instance count is: {s_count}");
+			}
+
+			~_41269Page()
+			{
+				Interlocked.Decrement(ref s_count);
+				Log.Warning("41269 Page", $"_41269 page instance count is: {s_count}");
+			}
+		}
+
+#if UITEST
+		//[Test]
+		//public void _41269Test()
+		//{
+		//	//RunningApp.WaitForElement(q => q.Marked(""));
+		//}
+#endif
+	}
+}
